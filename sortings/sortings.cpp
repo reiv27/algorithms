b42c143f09827289fb06b3e8ff378bd1b86c1d38
Roman Eidelman <reiv.dev@gmail.com>
2024-02-13 00:13:41 +0300

    init

---
+++ sortings/sortings.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+
+std::vector<int> BubbleSort(std::vector<int>& vector_) {
+  
+    int t = 0;
+        
+    for (size_t i=0; i < vector_.size(); ++i) {
+        for (size_t j=1; j < vector_.size()-i; ++j) {
+            if (vector_[j] < vector_[j-1]) {
+                t = vector_[j-1];
+                vector_[j-1] = vector_[j];
+                vector_[j] = t;
+                t = 0;
+            }
+        }
+    }
+        
+    return vector_;
+        
+};
+
+
+int main() {
+    
+    std::vector<int> nums = {4, 1, 0, 3, 10};
+    std::vector<int> test = {0, 1, 3, 4, 10};
+    
+    BubbleSort(nums);
+
+    if (BubbleSort(nums) == test) {
+    	std::cout << 1;
+    }
+    else std::cout << 0;
+ 
+}
